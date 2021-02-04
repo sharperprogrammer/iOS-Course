725614014d1fdebb80a100b43ccb92107fb1bff2
Seth Harper <sharperprogrammer@gmail.com>
2021-02-03 23:44:11 -0500

    Initial commit.

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Seth Harper on 1/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
